faf97751a8956b8bd50c6b0c48d251b94d168822
Michael <melole10124@gmail.com>
2025-08-13 17:01:57 +0100

    complete

--- ui.py
+++ ui.py
@@ -1,2 +1,53 @@
+from tkinter import *
+from quiz_brain import QuizBrain
+
 THEME_COLOR = "#375362"
 
+
+class QuizInterface:
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+        self.window = Tk()
+        self.window.title("Quizzlet")
+        self.window.config(background=THEME_COLOR,padx=20,pady=20)
+
+        self.score_label = Label(self.window,text=f"Score: 0", background=THEME_COLOR, fg="white")
+        self.score_label.grid(row=0,column=1)
+        self.canvas = Canvas(self.window, width=300, height=250)
+        self.text_box = self.canvas.create_text(150,125,width=280, text="Quiz Questions", font=("arial",20,"italic"))
+        self.canvas.grid(row=1,column=0,columnspan=2, pady=20)
+        
+        correctIMG = PhotoImage(file="images/true.png")
+        self.correctBTN = Button(image=correctIMG,command=lambda: self.check_answer("True"))
+        self.correctBTN.grid(row=2, column=0)
+        
+        wrongIMG = PhotoImage(file="images/false.png")
+        self.wrongtBTN = Button(image=wrongIMG,command=lambda: self.check_answer("False"))
+        self.wrongtBTN.grid(row=2, column=1)
+        
+        self.get_next_question()
+        
+        self.window.mainloop()
+    
+    def get_next_question(self):
+        self.canvas.config(bg="white")
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.text_box, text=q_text)
+        
+    def check_answer(self,answer : str):
+        self.get_score = self.quiz.check_answer(answer)
+        if len(self.quiz.question_list) <= 10:
+            self.score_label.config(text=f"Score: {self.get_score[0]}")
+            self.s = self.get_score[0]
+        else:
+            self.score_label.config(text=f"Score: {self.s}")
+        self.feedback(self.get_score[1])
+        self.window.after(500,self.get_next_question)
+        
+    
+    def feedback(self, a: bool):
+        if a:
+            self.canvas.config(bg="Green")
+        else:
+            self.canvas.config(bg="red")
+        
